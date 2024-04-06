9b98f5c68f5ab969b5e0088b46175fdd5c7f5b88
jainalphin <alphin.jain@forwoodsafety.com>
2024-04-06 11:46:42 +0530

    Added new scripts

--- run.sh
+++ run.sh
@@ -13,15 +13,16 @@ wget -O model.safetensors.index.json https://huggingface.co/AlphJain/llm/resolve
 wget -O config.json https://huggingface.co/AlphJain/llm/resolve/main/config.json?download=true
 wget -O generation_config.json https://huggingface.co/AlphJain/llm/resolve/main/generation_config.json?download=true
 
+mkdir models
 cd models
 mkdir model && mkdir tokenizer
 cd ..
 
-mv $PWD/model-00001-of-00002.safetensors $PWD/models/model/model-00001-of-00002.safetensors
-mv $PWD/model-00002-of-00002.safetensors $PWD/models/model/model-00002-of-00002.safetensors
-mv $PWD/tokenizer.json $PWD/models/tokenizer/tokenizer.json
-mv $PWD/tokenizer_config.json $PWD/models/tokenizer/tokenizer_config.json
-mv $PWD/tokenizer.model $PWD/models/tokenizer/tokenizer.model
-mv $PWD/model.safetensors.index.json $PWD/models/model/model.safetensors.index.json
-mv $PWD/config.json $PWD/models/model/config.json
-mv $PWD/generation_config.json $PWD/models/model/generation_config.json
+mv model-00001-of-00002.safetensors models/model/model-00001-of-00002.safetensors
+mv model-00002-of-00002.safetensors models/model/model-00002-of-00002.safetensors
+mv tokenizer.json models/tokenizer/tokenizer.json
+mv tokenizer_config.json models/tokenizer/tokenizer_config.json
+mv tokenizer.model models/tokenizer/tokenizer.model
+mv model.safetensors.index.json models/model/model.safetensors.index.json
+mv config.json models/model/config.json
+mv generation_config.json models/model/generation_config.json
